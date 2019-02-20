4da71170b246d0557ce92f614d454ead904bc769
Sandy Pan <sqp@andrew.cmu.edu>
2019-02-19 20:53:12 -0500

    Added Articles Model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ApplicationRecord
 	has_many :photos
+	belongs_to :article
 
-	scope :active, 			 -> { where('active = ?', true) }
-  scope :alphabetical, -> { order('name') }
+	scope :active, -> { where('active = ?', true) }
+  	scope :alphabetical, -> { order('name') }
   
 end
